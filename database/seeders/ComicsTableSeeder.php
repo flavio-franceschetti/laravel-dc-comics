a5b4f253138607ffa08fc305eb3ebc03a53488e9
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-09-16 16:58:43 +0200

    feat: add migration and seed database with data from config file

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+            $NewComic = new Comic();
+            $NewComic->title = $comic['title'];
+            $NewComic->description = $comic['description'];
+            $NewComic->thumb = $comic['thumb'];
+            $NewComic->price = $comic['price'];
+            $NewComic->series = $comic['series'];
+            $NewComic->sale_date = $comic['sale_date'];
+            $NewComic->type = $comic['type'];
+            $NewComic->save();
+        }
+
+    }
+}
